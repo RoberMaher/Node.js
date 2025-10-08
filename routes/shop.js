f96a70b0fcca1d1f4a9d14209ffe278fcbd6c565
robermaher <robermaher@gmail.com>
2025-10-08 16:48:21 +0300

    adding a layout to handlebars

--- routes/shop.js
+++ routes/shop.js
@@ -10,7 +10,12 @@ const router = express.Router();
 router.get('/', (req, res, next) => {
     const products = adminData.products;
     res.render('shop', {
-        prods: products, pageTitle: 'Shop', path: '/'});
+        prods: products, pageTitle: 'Shop',
+        path: '/',
+        hasProduct: products.length > 0,
+        activeShop: true,
+        product: true
+    });
 });
 
 
