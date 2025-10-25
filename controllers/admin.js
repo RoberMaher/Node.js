ba05323ad83022dd8d08269e78dfba1f5d9276a5
robermaher <robermaher@gmail.com>
2025-10-25 22:17:19 +0300

    Displaying Products Data

--- controllers/admin.js
+++ controllers/admin.js
@@ -12,11 +12,11 @@ exports.getAddProduct = (req, res, next) => {
 
 exports.postAddProduct = (req, res, next) => {
     const title = req.body.title;
-    const imageUr = req.body.imageUr;
+    const imageUrl = req.body.imageUrl;
     const price = req.body.price;
     const description = req.body.description;
     
-    const product = new Product(title, imageUr, description, price);
+    const product = new Product(title, imageUrl, description, price);
     product.save();
     res.redirect('/');
 };
