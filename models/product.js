0571dbfee5b61f7001f0bee387f93d06c3670f29
robermaher <robermaher@gmail.com>
2025-11-26 23:56:36 +0200

    Editing the Product Data

--- models/product.js
+++ models/product.js
@@ -15,10 +15,11 @@ const getProductsFromFile = cb => {
             cb(JSON.parse(fileContent));
         }
     });
-}
+};
 
 module.exports = class Product {
-    constructor(title, imageUrl, description, price) {
+    constructor(id, title, imageUrl, description, price) {
+        this.id = id;
         this.title = title;
         this.imageUrl = imageUrl;
         this.description = description;
@@ -26,12 +27,23 @@ module.exports = class Product {
     }
 
     save() {
-        this.id = Math.random().toString();
         getProductsFromFile(products => {
-            products.push(this);
-            fs.writeFile(p, JSON.stringify(products), err => {
-                console.log(err);
-            });
+            if (this.id) {
+                const existingProductIndex = products.findIndex(
+                    prod => prod.id === this.id
+                );
+                const updatedProducts = [...products];
+                updatedProducts[existingProductIndex] = this;
+                fs.writeFile(p, JSON.stringify(updatedProducts), err => {
+                    console.log(err);
+                });
+            } else {
+                this.id = Math.random().toString();
+                products.push(this);
+                fs.writeFile(p, JSON.stringify(products), err => {
+                    console.log(err);
+                });
+            }
         });
     }
 
@@ -39,7 +51,7 @@ module.exports = class Product {
         getProductsFromFile(cb);
     }
 
-    static findById( id, cb) {
+    static findById(id, cb) {
         getProductsFromFile(products => {
             const product = products.find(p => p.id === id);
             cb(product);
--- routes/admin.js
+++ routes/admin.js
@@ -6,8 +6,6 @@ const adminController = require('../controllers/admin');
 
 const router = express.Router();
 
-const products = [];
-
 // /admin/add-product => GET
 router.get('/add-product', adminController.getAddProduct);
 
@@ -18,5 +16,7 @@ router.get('/products', adminController.getProducts);
 router.post('/add-product', adminController.postAddProduct);
 
 router.get('/edit-product/:productId', adminController.getEditProduct);
+
+router.post('/edit-product', adminController.postEditProduct);
 
 module.exports = router;
